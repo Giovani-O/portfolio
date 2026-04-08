5de8202273ddf4b4321c175bdda9d6d57ee6eb04
Giovani <giovani98@proton.me>
2026-04-08 15:07:38 -0300

    refact: Change component order

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,37 +1,39 @@
-import { BasePanel } from '../../components/base-panel'
-import { Footer } from '../../components/footer'
-import { Separator } from '../../components/separator'
-import { Spacer } from './style'
-import { lazy, Suspense } from 'react'
+import { BasePanel } from "../../components/base-panel";
+import { Footer } from "../../components/footer";
+import { Separator } from "../../components/separator";
+import { Spacer } from "./style";
+import { lazy, Suspense } from "react";
 
-const About = lazy(() => import('../about').then((m) => ({ default: m.About })))
+const About = lazy(() =>
+	import("../about").then((m) => ({ default: m.About })),
+);
 const Experience = lazy(() =>
-  import('../experience').then((m) => ({ default: m.Experience }))
-)
+	import("../experience").then((m) => ({ default: m.Experience })),
+);
 const Projects = lazy(() =>
-  import('../projects').then((m) => ({ default: m.Projects }))
-)
+	import("../projects").then((m) => ({ default: m.Projects })),
+);
 
 export function Home() {
-  return (
-    <main id="home">
-      <Spacer />
-      <BasePanel>
-        <Suspense fallback={<p>Loading...</p>}>
-          <About />
-        </Suspense>
-        <Separator />
+	return (
+		<main id="home">
+			<Spacer />
+			<BasePanel>
+				<Suspense fallback={<p>Loading...</p>}>
+					<About />
+				</Suspense>
+				<Separator />
 
-        <Suspense fallback={<p>Loading...</p>}>
-          <Experience />
-        </Suspense>
-        <Separator />
+				<Suspense fallback={<p>Loading...</p>}>
+					<Projects />
+				</Suspense>
+				<Separator />
 
-        <Suspense fallback={<p>Loading...</p>}>
-          <Projects />
-        </Suspense>
-      </BasePanel>
-      <Footer />
-    </main>
-  )
+				<Suspense fallback={<p>Loading...</p>}>
+					<Experience />
+				</Suspense>
+			</BasePanel>
+			<Footer />
+		</main>
+	);
 }
