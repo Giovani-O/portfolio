2b92caa2244a3ef0ecd344e6a909011f1dfe7b6f
Giovani <giovani98@proton.me>
2026-04-07 21:40:29 -0300

    Updates

--- assets/index-BrQIH29A.js
+++ assets/index-BrQIH29A.js
@@ -1,4 +1,4 @@
-import{d as i,u as r,j as e,b as a}from"./main-C96fd_ja.js";const l=i.div`
+import{d as i,u as r,j as e,b as a}from"./main-DP8QtMx6.js";const l=i.div`
   display: flex;
   flex-direction: column;
   align-items: center;
--- assets/index-Fo18dGWj.js
+++ assets/index-Fo18dGWj.js
@@ -1,4 +1,4 @@
-import{d as o,R as s,j as t,u as a,a as l,S as n}from"./main-C96fd_ja.js";const r=o.div`
+import{d as o,R as s,j as t,u as a,a as l,S as n}from"./main-DP8QtMx6.js";const r=o.div`
   position: relative;
   display: flex;
   flex-direction: column;
